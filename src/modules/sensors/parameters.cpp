1594ef2a6096dfb8a31dc8a4f7171357163bd93e
klopezal <kevin.lopez.alvarez@drotek.com>
2016-12-15 19:02:30 +0100

    Added v4pro ADC parameter config

--- src/modules/sensors/parameters.cpp
+++ src/modules/sensors/parameters.cpp
@@ -409,6 +409,8 @@ int update_parameters(const ParameterHandles &parameter_handles, Parameters &par
 		/* apply scaling according to defaults if set to default */
 #if defined (CONFIG_ARCH_BOARD_PX4FMU_V4)
 		parameters.battery_v_div = 13.653333333f;
+#elif defined (CONFIG_ARCH_BOARD_PX4FMU_V4PRO)
+		parameters.battery_v_div = 13.653333333f;
 #elif defined (CONFIG_ARCH_BOARD_PX4FMU_V2) || defined ( CONFIG_ARCH_BOARD_MINDPX_V2 )
 		parameters.battery_v_div = 10.177939394f;
 #elif defined (CONFIG_ARCH_BOARD_AEROCORE)
@@ -435,6 +437,9 @@ int update_parameters(const ParameterHandles &parameter_handles, Parameters &par
 #if defined (CONFIG_ARCH_BOARD_PX4FMU_V4)
 		/* current scaling for ACSP4 */
 		parameters.battery_a_per_v = 36.367515152f;
+#elif defined (CONFIG_ARCH_BOARD_PX4FMU_V4PRO)
+		/* current scaling for TODO */
+		parameters.battery_a_per_v = 15.391030303f;
 #elif defined (CONFIG_ARCH_BOARD_PX4FMU_V2) || defined (CONFIG_ARCH_BOARD_MINDPX_V2) || defined (CONFIG_ARCH_BOARD_AEROCORE) || defined (CONFIG_ARCH_BOARD_PX4FMU_V1)
 		/* current scaling for 3DR power brick */
 		parameters.battery_a_per_v = 15.391030303f;
