5385618406574f1f8df613341fd7293cd7fe743f
klopezal <kevin.lopez.alvarez@drotek.com>
2017-01-27 11:08:48 +0100

    px4fmu-v4pro : correct ADC configuration and power supply sensing

--- src/drivers/boards/px4fmu-v4pro/board_config.h
+++ src/drivers/boards/px4fmu-v4pro/board_config.h
@@ -196,10 +196,10 @@ __BEGIN_DECLS
 // ADC defines to be used in sensors.cpp to read from a particular channel
 #define ADC_BATTERY_VOLTAGE_CHANNEL		2
 #define ADC_BATTERY_CURRENT_CHANNEL		3
-//#define ADC_BATTERY_VOLTAGE_CHANNEL2	4
-//#define ADC_BATTERY_CURRENT_CHANNEL2	5
 #define ADC_5V_RAIL_SENSE				4
-#define ADC_RC_RSSI_CHANNEL				11
+#define ADC_BATTERY2_VOLTAGE_CHANNEL	11
+#define ADC_BATTERY2_CURRENT_CHANNEL	13
+//#define ADC_RC_RSSI_CHANNEL				11
 
 /* Define Battery 1 Voltage Divider and A per V
  */
